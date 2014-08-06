b680d9a3ec647a277ceefc9ac0d08daa29e0cc4b
Joe Kearney <joe.j.kearney@gmail.com>
2014-08-06 10:08:54 +0100

    hide constructor

--- src/test/java/com/google/common/util/concurrent/PackagePrivateAccessorForGuava.java
+++ src/test/java/com/google/common/util/concurrent/PackagePrivateAccessorForGuava.java
@@ -3,6 +3,8 @@ package com.google.common.util.concurrent;
 import java.util.concurrent.Executor;
 
 public class PackagePrivateAccessorForGuava {
+    private PackagePrivateAccessorForGuava() {}
+    
     public static SerializingExecutor newSerializingExecutor(Executor executor) {
         return new SerializingExecutor(executor);
     }
